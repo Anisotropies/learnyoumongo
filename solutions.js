4fa0d30f67e278e0fc12820795854540adc5e942
Anisotropies <Anisotropies@users.noreply.github.com>
2018-03-06 21:54:30 -0800

    Added Exercises 5,6,7,8,9

--- solutions.js
+++ solutions.js
@@ -44,4 +44,111 @@
         db.close()
       })
     })
+//Exercise5.js
+  var mongo = require('mongodb').MongoClient
+    
+    var firstName = process.argv[2]
+    var lastName = process.argv[3]
+    var doc = {
+      firstName: firstName
+    , lastName: lastName
+    }
+    
+    var url = 'mongodb://localhost:27017/learnyoumongo'
+    mongo.connect(url, function(err, db) {
+      if (err) throw err
+      var collection = db.collection('docs')
+      collection.insert(doc, function(err, data) {
+        if (err) throw err
+        console.log(JSON.stringify(doc))
+        db.close()
+      })
+    })
+
+//Exercise6.js
+   var mongo = require('mongodb').MongoClient
+    
+    var url = 'mongodb://localhost:27017/' + process.argv[2]
+    mongo.connect(url, function(err, db) {
+      if (err) throw err
+      var collection = db.collection('users')
+      collection.update({
+        username: 'tinatime'
+      }, {
+        $set: {
+          age: 40
+        }
+      }, function(err) {
+        if (err) throw err
+        db.close()
+      })
+    })
+//Exercise7.js
+────────────────────────────────────────────────────────────────────────────
+    var mongo = require('mongodb').MongoClient
+    
+    var url = 'mongodb://localhost:27017/' + process.argv[2]
+    
+    mongo.connect(url, function(err, db) {
+      if (err) throw err
+      var collection = db.collection(process.argv[3])
+      collection.remove({
+        _id: process.argv[4]
+      }, function(err) {
+        if (err) throw err
+        db.close()
+      })
+    })
+    
+
+//Exericse8.js
+    var mongo = require('mongodb').MongoClient
+    var age = process.argv[2]
+    
+    var url = 'mongodb://localhost:27017/learnyoumongo'
+    
+    mongo.connect(url, function(err, db) {
+      if (err) throw err
+      var parrots = db.collection('parrots')
+      parrots.count({
+        age: {
+          $gt: +age
+        }
+      }, function(err, count) {
+        if (err) throw err
+        console.log(count)
+        db.close()
+      })
+    })
+    
+//Exercise9.js
+ var mongo = require('mongodb').MongoClient
+    var size = process.argv[2]
+    
+    var url = 'mongodb://localhost:27017/learnyoumongo'
+    
+    mongo.connect(url, function(err, db) {
+      if (err) throw err
+      var prices = db.collection('prices')
+      prices.aggregate([
+        { $match: {
+          size: size
+        }}
+      , { $group: {
+          _id: 'average'
+        , average: {
+            $avg: '$price'
+          }
+        }}
+      ]).toArray(function(err, results) {
+        if (err) throw err
+        if (!results.length) {
+          throw new Error('No results found')
+        }
+        var o = results[0]
+        console.log(Number(o.average).toFixed(2))
+        db.close()
+      })
+    })
+
 
